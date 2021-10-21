b7d678bf3571cd6375e74fc5f3c9583df767eb1d
David Hong <david.hong199@gmail.com>
2021-10-20 17:01:07 -0700

    established connection to mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 require("dotenv").config({ path: "./config.env" });
+
+const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-// app.use(require("./routes/record"));
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const socialRouter = require("./routes/social");
+app.use("/social", socialRouter);
 
 // const dbo = require("./db/conn");
 
